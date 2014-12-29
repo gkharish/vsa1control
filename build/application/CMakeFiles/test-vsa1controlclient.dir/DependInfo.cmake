b3a47bd3c269f9b7ac069697463a7e26a3a39946
Ganesh Kumar <gkharish@laas.fr>
2014-12-29 03:13:21 +0100

    build added

---
+++ build/application/CMakeFiles/test-vsa1controlclient.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/users/localuser/softdev/vsa1control/application/vsa1control.cpp" "/usr/users/localuser/softdev/vsa1control/build/application/CMakeFiles/test-vsa1controlclient.dir/vsa1control.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/usr/users/localuser/softdev/vsa1control/build/src/CMakeFiles/vsa1control.dir/DependInfo.cmake"
+  )
---
+++ build/application/CMakeFiles/test-vsa1controlclient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test-vsa1controlclient.dir/vsa1control.cpp.o"
+  "test-vsa1controlclient.pdb"
+  "test-vsa1controlclient"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test-vsa1controlclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
