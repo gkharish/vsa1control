36ed1d65d1422d74036daba119a9d282ffdf705b
Ganesh Kumar <gkharish@laas.fr>
2014-12-30 00:36:25 +0100

    fixing bug

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/users/localuser/softdev/vsa1control
-# Build directory: /usr/users/localuser/softdev/vsa1control/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(application)
